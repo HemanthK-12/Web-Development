af659dcbfed7010a0bb4378db452cf01f06239b7
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-07 19:42:09 +0530

    array manipulation and functions

--- Practice/Javascript Practice/script.js
+++ Practice/Javascript Practice/script.js
@@ -1,5 +1,10 @@
-let fullName=prompt("Enter your full name and I will generate your username");
-fullName=fullName.replaceAll(" ","");
-fullName=fullName.toLowerCase();
-let username=`@`.concat(fullName.concat(String(fullName.length)));
-alert(`Your username is : ${username}`);
+let company=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(company);
+company.splice(0,1);
+console.log(company);
+
+company.splice(1,1,"Ola");
+console.log(company);
+
+company.push("Amazon");
+console.log(company);
