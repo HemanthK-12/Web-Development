89e0c65661643f58d8173734c73f0a2d23f2e690
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-07 15:20:15 +0530

    prompt,alert and confirm

--- Practice/Javascript Practice/script.js
+++ Practice/Javascript Practice/script.js
@@ -1,22 +1,17 @@
-const product=
-{
-    productName : "Parker Jotter Standard CT Ball Pen(Black)",
-    productRating : 4,
-    productReviews : 7002,
-    productPrice : 270,
-    productOffer : 5
-};
+grade=prompt("Enter ypur marks")
 
-const profile =
-{
-    userID : "shradhakapra",
-    posts : 195,
-    followers : "569k",
-    following : 4,
-    profileName : "Shradha Kapra",
-    threadsName : "shradhakapra",
-    profilePicLink : "https://www.instagram.com/p/CT9Z9Z9t9Z9/",
-    bio : "Apna College | Ex-Microsoft ",
-    role : "Entrepreneur"
-};
-console.log(profile)
+if(grade<=100 && grade>=80){
+    console.log("A")
+}
+else if(grade<=89 && grade>=70){
+    console.log("B")
+}
+else if(grade<=69 && grade>=60){
+    console.log("C")
+}
+else if(grade<=59 && grade>=50){
+    console.log("D")
+}
+else if(grade<=49 && grade>=0){
+    console.log("F")
+}
