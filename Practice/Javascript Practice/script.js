da5a0e8a818991683feb403009dbba55d4ad5c02
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-07 15:17:14 +0530

    variables and operators

---
+++ Practice/Javascript Practice/script.js
@@ -0,0 +1,22 @@
+const product=
+{
+    productName : "Parker Jotter Standard CT Ball Pen(Black)",
+    productRating : 4,
+    productReviews : 7002,
+    productPrice : 270,
+    productOffer : 5
+};
+
+const profile =
+{
+    userID : "shradhakapra",
+    posts : 195,
+    followers : "569k",
+    following : 4,
+    profileName : "Shradha Kapra",
+    threadsName : "shradhakapra",
+    profilePicLink : "https://www.instagram.com/p/CT9Z9Z9t9Z9/",
+    bio : "Apna College | Ex-Microsoft ",
+    role : "Entrepreneur"
+};
+console.log(profile)
