1bda192a368b4c8aa5a19ea875f0a1a7b7447495
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-07 19:26:53 +0530

    convert name to username using string functions

--- Practice/Javascript Practice/script.js
+++ Practice/Javascript Practice/script.js
@@ -1,15 +1,5 @@
-let digits=prompt("Welcoe to the Guessing Game! Type the number of digits in the number you want to guess");
-let number=prompt("Guess the number which I'm thinking!");
-let random=Math.ceil(Math.random()*(10**(digits))+1);
-
-while(true)
-    {
-        if(number!=random)
-            number=prompt("No,Try again");
-        else
-            {
-                alert("Congratulations! You have guessed the number correctly!");
-                break;
-            }
-
-    }
+let fullName=prompt("Enter your full name and I will generate your username");
+fullName=fullName.replaceAll(" ","");
+fullName=fullName.toLowerCase();
+let username=`@`.concat(fullName.concat(String(fullName.length)));
+alert(`Your username is : ${username}`);
