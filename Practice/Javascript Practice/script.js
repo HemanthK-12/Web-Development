9f67471c5a5cc24e1a5056e06b4c5bb9c98c39de
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-09 15:32:37 +0530

    forEach loop example

--- Practice/Javascript Practice/script.js
+++ Practice/Javascript Practice/script.js
@@ -1,16 +1,6 @@
-const numberVowels=(x)=>
-{
-    let vowels="aeiouAEIOU";
-    let n=0;
-    for(let i=0;i<x.length;i++)
-    {
-        for(let j=0;j<vowels.length;j++)
-        {
-            if(x.charAt(i)==vowels.charAt(j))
-                n++;
-        }
-    }
-    return n;
-}
-let str=prompt("Enter a string to find out the nuumber of vowels");
-alert("The number of vowels in the string is "+numberVowels(str));
+str=prompt("Enter the elements of the array separated by commas");
+arr=str.split(",");
+
+arr.forEach((x)=>{
+    console.log(x**2);
+})
