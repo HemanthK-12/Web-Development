879586a259cb4a9f89b966e3140710357d6d622d
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-10 22:49:43 +0530

    editing div contents of html through js

--- Practice/Javascript Practice/DOM/script.js
+++ Practice/Javascript Practice/DOM/script.js
@@ -1,4 +1,7 @@
-let str=document.querySelector("h2").innerText;
-str=str.concat(" from Apna College");
-//document.querySelector("h2").innerText=str;
-document.querySelector("h2").innerText=document.querySelector("h2").innerText.concat(" from Apna College")
+let divCollection=document.querySelectorAll("div");
+
+divCollection[0].innerText="Hiii";
+divCollection[1].innerText="Hello";
+divCollection[2].innerText="How are You?";
+
+console.log(divCollection);
