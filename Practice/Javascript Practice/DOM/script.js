0e1b1fdc40a6ab1836a285cc1ce16397cafb4f2a
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-06-10 22:34:32 +0530

    editing html with javascript

---
+++ Practice/Javascript Practice/DOM/script.js
@@ -0,0 +1,4 @@
+let str=document.querySelector("h2").innerText;
+str=str.concat(" from Apna College");
+//document.querySelector("h2").innerText=str;
+document.querySelector("h2").innerText=document.querySelector("h2").innerText.concat(" from Apna College")
